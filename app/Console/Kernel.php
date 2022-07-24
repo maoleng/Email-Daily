88612675038defd26d39a876125ac1ed873b5f24
Bùi Hữu Lộc <feature451@gmail.com>
2022-07-25 00:41:38 +0700

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,9 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
+    protected $commands = [
+        Commands\LoopSendMail::class,
+    ];
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('command:send_mail')->everyThirtyMinutes();
