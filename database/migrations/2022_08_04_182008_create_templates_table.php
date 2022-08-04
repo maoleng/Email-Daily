511d420fc5f71044ee3aad593f1ee7689fa67839
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-05 01:29:23 +0700

    template and schedule

---
+++ database/migrations/2022_08_04_182008_create_templates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250);
+            $table->longText('content');
+            $table->string('sender', 250)->nullable();
+            $table->string('cron_time', 20);
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('templates');
+    }
+};
