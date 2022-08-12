bb7ca3b78107085bef5202fd8ad7709e2bd1d518
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-13 02:51:24 +0700

    config model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Config;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Config::query()->insert([
+            [
+                'key' => 'register_title',
+                'value' => 'XÁC NHẬN ĐĂNG KÝ',
+            ],
+            [
+                'key' => 'facebook_url',
+                'value' => 'https://www.facebook.com/maolengg',
+            ],
+            [
+                'key' => 'github_url',
+                'value' => 'https://github.com/apps/maoleng',
+            ],
+            [
+                'key' => 'gitlab_url',
+                'value' => 'https://gitlab.com/maoleng',
+            ],
+            [
+                'key' => 'google_url',
+                'value' => 'feature451@gmail.com',
+            ],
+            [
+                'key' => 'linkedin_url',
+                'value' => 'https://www.linkedin.com/company/maoleng',
+            ],
+        ]);
     }
 }
