3fbb09b54c277fd801630805407fd90319f64e4d
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-21 22:29:45 +0700

    title

--- resources/views/app-theme/header.blade.php
+++ resources/views/app-theme/header.blade.php
@@ -33,7 +33,7 @@
 
     <div class="intro-x dropdown w-8 h-8">
         <div class="dropdown-toggle w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in" role="button" aria-expanded="false" data-tw-toggle="dropdown">
-            <img alt="Midone - HTML Admin Template" src="{{authed()->avatar}}">
+            <img alt="MaoLeng - EmailDaily" src="{{authed()->avatar}}">
         </div>
         <div class="dropdown-menu w-56">
             <ul class="dropdown-content bg-primary text-white">
--- resources/views/app-theme/page.blade.php
+++ resources/views/app-theme/page.blade.php
@@ -2,6 +2,6 @@
     <meta charset="utf-8">
     <link href="" rel="shortcut icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard - Midone - Tailwind HTML Admin Template</title>
+    <title>Email Daily</title>
     <link rel="stylesheet" href="{{asset('app/css/app.css')}}" />
 </head>
